0d59bbda2bc7887e5f687cb052ce7be5506a6a98
shibil885 <shibilmuhammed885@gmail.com>
2025-01-30 18:42:31 +0530

    fix: logout issue fixed

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
   isLoggedIn(): boolean {
     return !!localStorage.getItem('acc_T');
   }
 
   logout() {
-    localStorage.removeItem('acc_T')
+    localStorage.removeItem('acc_T');
+    this.router.navigate(['auth'])
   }
 }
